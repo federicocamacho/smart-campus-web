8fb393bf3da7fb6871ce4dd32b6fbc1f1dd35a76
Jose David Rojas Aguilar <josedrojasa@hotmail.com>
2019-03-10 20:57:36 +0000

    Update environment.prod.ts

--- web-application/src/environments/environment.prod.ts
+++ web-application/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endpoint: 'http://localhost:8090',
+  endpoint: 'https://apiuisiot.serveo.net',
   timeout: 50000,
   user: '/users/user',
   authentication: '/users/authentication',
