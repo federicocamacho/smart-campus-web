1f0068872d1923babbe4a20426df663ffcf40141
Federico Camacho <federicocamacho97@hotmail.com>
2019-04-04 19:55:28 -0500

    Progress in Applications list.

--- smart-campus/src/app/main/pages/applications/applications.component.ts
+++ smart-campus/src/app/main/pages/applications/applications.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
+/**
+ * Page to manage all the user's applications.
+ *
+ * @date 2019-04-03
+ * @export
+ */
 @Component({
   selector: 'sc-applications',
   templateUrl: './applications.component.html',
   styleUrls: ['./applications.component.css']
 })
-export class ApplicationsComponent implements OnInit {
+export class ApplicationsComponent {
 
-  constructor() { }
+  /**
+   * Creates an instance of ApplicationsComponent.
+   * @date 2019-04-03
+   * @param activatedRoute - current Route.
+   * @param router - Angular router.
+   */
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
 
-  ngOnInit() {
+  /**
+   * Trigerred when pressing "Create app" button.
+   *
+   * @date 2019-04-03
+   */
+  public createApplication(): void {
+    this.router.navigate([ '0' ], { relativeTo: this.activatedRoute });
   }
 
 }
