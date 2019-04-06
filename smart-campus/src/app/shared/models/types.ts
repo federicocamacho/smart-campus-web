2dbb1d6cb2e80b942afc4ba676cb6884124f794e
Federico Camacho <federicocamacho97@hotmail.com>
2019-04-06 17:39:04 -0500

    Gateway filters type.

--- smart-campus/src/app/shared/models/types.ts
+++ smart-campus/src/app/shared/models/types.ts
@@ -3,3 +3,4 @@ export type ThingType = 'GATEWAY' | 'SENSOR' | 'ACTUATOR';
 
 // Filter types.
 export type ApplicationsFilter = 'NAME' | 'DESCRIPTION' | 'NONE';
+export type GatewaysFilter = 'NAME' | 'DESCRIPTION' | 'IP' | 'IS_ALIVE' | 'NONE';
