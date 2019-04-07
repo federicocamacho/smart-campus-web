b1d86f04562373211b448df1a7e5e1f17586395f
Federico Camacho <federicocamacho97@hotmail.com>
2019-04-07 14:54:48 -0500

    Improve gateways table in application.

--- smart-campus/src/app/main/components/gateways-by-application/gateways-by-application.component.ts
+++ smart-campus/src/app/main/components/gateways-by-application/gateways-by-application.component.ts
@@ -27,14 +27,11 @@ export class GatewaysByApplicationComponent extends DataTable<Gateway, GatewaysF
 
   protected filterPredicate: (data: Gateway, filter: string) => boolean = (data: Gateway, filter: string) => {
     switch (this.filterType) {
-      case 'DESCRIPTION':
-        return Util.stringContains(data.description, filter);
-      case 'IP':
-        return Util.stringContains(data.ip, filter);
-      case 'IS_ALIVE':
-        return data.alive === (filter === 'true');
-      case 'NONE':
-        return true;
+      case 'NAME': return Util.stringContains(data.name, filter);
+      case 'DESCRIPTION': return Util.stringContains(data.description, filter);
+      case 'IP': return Util.stringContains(data.ip, filter);
+      case 'IS_ALIVE': return data.alive === (filter === 'true');
+      default: return true;
     }
   }
 
