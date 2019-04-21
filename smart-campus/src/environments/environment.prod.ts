a6c1912f2d7d20c422f4bf620819967b3c96d7a6
Jose David Rojas Aguilar <josedrojasa@hotmail.com>
2019-04-21 19:06:49 +0000

    Update environment.prod.ts

--- smart-campus/src/environments/environment.prod.ts
+++ smart-campus/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   adminService: 'http://fe6d7de4.ngrok.io',
   dataService: 'http://fe6d7de4.ngrok.io',
-  brokerUrl : 'tcp://0.tcp.ngrok.io:10084'
+  brokerUrl : 'ws://0.tcp.ngrok.io:10084/ws'
 };
