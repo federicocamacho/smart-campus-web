8fc94356466647fd70f62fd51f3c6c518de2047f
Jose David Rojas Aguilar <josedrojasa@hotmail.com>
2019-04-21 15:30:40 +0000

    Update environment.prod.ts

--- smart-campus/src/environments/environment.prod.ts
+++ smart-campus/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  adminService: 'http://apiuisiot.serveo.net',
-  dataService: 'http://apiuisiot.serveo.net',
+  adminService: 'http://45236916.ngrok.io',
+  dataService: 'http://45236916.ngrok.io',
   brokerUrl : 'tcp://localhost:61616'
 };
