a7280ab102bd291651f5436de6507a754e1e227c
Federico Camacho <federicocamacho97@hotmail.com>
2019-05-13 20:48:57 -0500

    Fix dev connection to broker via MQTT, fix wrong day info shown in changes statistics.

--- smart-campus/src/environments/environment.prod.ts
+++ smart-campus/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   adminService: 'http://d0e9d920.ngrok.io',
   dataService: 'http://d0e9d920.ngrok.io',
-  brokerUrl : 'ws://0.tcp.ngrok.io:10827/ws'
+  brokerUrl : '0.tcp.ngrok.io',
+  brokerPort: 10827,
 };
